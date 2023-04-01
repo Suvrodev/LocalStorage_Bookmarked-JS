1be0f7e24e88a3bdf176ce46d228d8698a639759
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-04-02 00:18:27 +0600

    Bookmarked worked done and set sweet alert

--- JS/app.js
+++ JS/app.js
@@ -9,12 +9,12 @@ const loadProduct = () => {
     data.forEach((product) => {
       const card = document.createElement("div");
       card.classList.add("card", "m-2");
-      
+
       card.innerHTML = `
             <div class="bookmark-icon">
           
             <i class="fa-solid fa-bookmark"></i>
-            <i class="fa-regular fa-bookmark"></i>
+            <i onclick="handleBookmark('${product.id}','${product.name}','${product.price}')" class="fa-regular fa-bookmark"></i>
             
           </div>
           <div class="product-img-container">
@@ -36,6 +36,39 @@ const loadProduct = () => {
   };
   
   // ! handle book mark
+
+  const handleBookmark=(id,name,price)=>{
+    // console.log('ID: '+id);
+    // console.log('Name: '+name);
+    // console.log('Price: '+price)
+    const product={id,name,price}
+    let Bookmark=[];
+
+    const PreviousBookmark=JSON.parse(localStorage.getItem('myBooMarked'));
+    if(PreviousBookmark){
+        //console.log(PreviousBookmark)
+        const isThisPRoductMarked=PreviousBookmark.find(pd=>pd.id===id)
+        if(isThisPRoductMarked){
+            Swal.fire({
+                icon:"error",
+                title: "Oops...",
+                text:"Already bookdmarked",
+                footer: "This Product Already Bookmarked"
+            }
+        )
+           //alert('This Product Already Bookmarked')
+        }else{
+            console.log('This product not exists')
+            Bookmark.push(...PreviousBookmark,product)
+            localStorage.setItem('myBooMarked',JSON.stringify(Bookmark))
+        }
+
+    }else{
+       Bookmark.push(product) 
+       localStorage.setItem('myBooMarked',JSON.stringify(Bookmark))
+    }
+   
+  }
   
   
   loadProduct();
